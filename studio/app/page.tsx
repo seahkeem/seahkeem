ac85f7e9329abbad1ae232cafb212e43577916a8
Seah Kim <seahkeem@gmail.com>
2026-02-23 17:26:49 +0900

    chore: initial project setup

---
+++ studio/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="font-mono text-3xl font-semibold">
+        Welcome to @seahkeem Studio
+      </h1>
+    </main>
+  );
+}
